62c2df3ed34a45db0378db502d3084b9bdc55664
Anders Pierson <apierso3@uncc.edu>
2023-12-14 16:39:36 -0500

    whatever

--- Working_Folder/phase1.py
+++ Working_Folder/phase1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-plyr_salary = pd.read_csv("C:\\Users\\apier\\OneDrive\\Documents\DSBA\5122\NBA_Most_Valuable_Player_By_The_Numbers\Dataset\RankedPlayersbySalary.csv")
+plyr_salary = pd.read_csv("Dataset\RankedPlayersbySalary.csv", encoding="unicode_escape")
 
 plyr_salary.head()
