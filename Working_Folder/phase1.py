fdbf0672f86f0a74be558d4757a02adf8cd663bd
Anders Pierson <apierso3@uncc.edu>
2023-12-14 03:01:56 -0500

    addnewcsv

--- Working_Folder/phase1.py
+++ Working_Folder/phase1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-plyr_salary = pd.read_csv("RankedPlayersbySalary.csv")
+plyr_salary = pd.read_csv("RankedPlayersbySalaryWorking.csv")
 
 plyr_salary.head()
