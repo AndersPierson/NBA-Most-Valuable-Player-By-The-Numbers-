61d43951a09bc204675561c2238034715ac61cd5
Anders Pierson <apierso3@uncc.edu>
2023-12-14 16:49:26 -0500

    Reading in data and assigning them to pandas dataframes.

--- Working_Folder/phase1.py
+++ Working_Folder/phase1.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
+
 
 plyr_salary = pd.read_csv("Dataset\RankedPlayersbySalary.csv", encoding="unicode_escape")
+plyr_points = pd.read_csv("Dataset\RankedPlayersbyPoints.csv", encoding='unicode_escape')
+plyr_assists = pd.read_csv("Dataset\RankedPlayersbyAssists.csv", encoding='unicode_escape')
+plyr_rebounds = pd.read_csv("Dataset\RankedPlayersbyRebounds.csv", encoding='unicode_escape')
 
-plyr_salary.head()
