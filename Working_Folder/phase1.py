33a810bec024010f53f9285f0d10543af1e26948
Anders Pierson <apierso3@uncc.edu>
2023-12-14 02:50:59 -0500

    change specifics

--- Working_Folder/phase1.py
+++ Working_Folder/phase1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-plyr_salary = pd.read_csv("Dataset/RankedPlayersbySalary.csv")
+plyr_salary = pd.read_csv("RankedPlayersbySalary.csv")
 
 plyr_salary.head()
