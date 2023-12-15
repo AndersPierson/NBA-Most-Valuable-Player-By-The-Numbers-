68e43562e812bcf0761abdf866b110c3dc974346
Anders Pierson <apierso3@uncc.edu>
2023-12-14 23:29:08 -0500

    Cleaning up some more

---
+++ Working_Folder/pages/4_Conclusions.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.write("# Conclusions From This Project:")
+
+st.markdown(
+    """
+    It can be discerned from our analysis, that out of the three metrics used in our project there are two that stick out in particular...
+
+    **Points Per Game:**
+
+    This metric is severly over valued in the current market. Top shooters in the league currently are underperforming their current salaries by
+    an average of around $60,000 dollars and the other two metrics (Assists and Rebounds per Game) do not come close to hitting that mark.
+
+    **Rebounds Per Game:**
+
+    This metric was astounding to see have the effect that is does, especially since is has not actual weighted scale when it comes to fantasy grading
+    of players Not only was the average performance of players according to their salaries right around the break even line (over by $13,000), 
+    the bigger contracts have predictable outcoems as almost all of them are massively underperforming in the current season so far!
+"""
+)
